6a86c2e9d3110b779e80d438177722dacb014506
merri4 <kjhwang97@gmail.com>
2024-01-29 22:31:15 +0900

    feat: 로직 함수화, arg 추가

--- code/relabel.py
+++ code/relabel.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch
+import argparse
 from tqdm import tqdm
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from collections import Counter
@@ -14,18 +15,30 @@ def check_same(row):
     return values[0]
   else:
     return -1
+  
+
+def parse_arguments() :
+
+  parser = argparse.ArgumentParser(description='Argparse')
+
+  parser.add_argument('--batch_size', type=int, default=128)
+  parser.add_argument('--target_dir', type=str, default='../data/train.csv')
+  parser.add_argument('--output_dir', type=str, default='../relabel.csv')
+  
+  args = parser.parse_args()
+
+  return args
 
 
-INPUT_PATH = '/content/drive/MyDrive/Level2_Topic/'
-gemini_gen = pd.read_csv(f'{INPUT_PATH}gemini_generate_V2.csv')
+args = parse_arguments()
 
-# Batch the data
-batch_size = 128
-gemini_batches = [gemini_gen[i:i+batch_size] for i in range(0, len(gemini_gen), batch_size)]
+df_org = pd.read_csv(args.target_dir)
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 DEVICE
 
+# Batch the data
+batches = [df_org[i:i+args.batch_size] for i in range(0, len(df_org), args.batch_size)]
 model_names = [
     "yobi/klue-roberta-base-ynat",
     "JiHoon-kim/bert-base-klue-ynat-finetuned",
@@ -42,7 +55,7 @@ for model_name in model_names :
 
     # Perform inference on each batch
     preds = []
-    for batch in tqdm(gemini_batches):
+    for batch in tqdm(batches):
         inputs = tokenizer(batch['text'].tolist(), return_tensors="pt", padding=True, truncation=True).to(DEVICE)
         with torch.no_grad():
             logits = model(**inputs).logits
@@ -50,17 +63,18 @@ for model_name in model_names :
             preds.extend(pred)
 
     # Update the test data with the predictions
-    gemini_gen['target_{}'.format(model_name.split("/")[0])] = preds
+    df_org['target_{}'.format(model_name.split("/")[0])] = preds
 
 
 
 # 만장일치된 값으로 target 수정. (target drop하고 target_final을 target으로 rename해줌.)
-gemini_gen['target_final'] = gemini_gen.apply(check_same, axis=1)
+df_org['target_final'] = df_org.apply(check_same, axis=1)
 
 # 만장일치 된것만 남기기
-gemini_sure = gemini_gen[gemini_gen['target_final'] != -1]
-gemini_sure.drop('target', axis=1, inplace=True)
-gemini_sure.rename(columns={'target_final': 'target'}, inplace=True)
+df_sure = df_org[df_org['target_final'] != -1]
+df_sure.drop('target', axis=1, inplace=True)
+df_sure.rename(columns={'target_final': 'target'}, inplace=True)
 
 # 저장
-gemini_sure.to_csv(f'{INPUT_PATH}gemini_relabel.csv', index=False)
+df_sure.to_csv(args.output_dir, index=False)
+print("Done!")
